9bb937277a1ab37298db9e94099f9b22a989afeb
Meg Ducharme <ducharme.meg@gmail.com>
2016-08-26 15:31:38 -0500

    Added Templates for Handlebars

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
 "use strict";
 
+var movieData = {
+
+}
 
 function getMovieInfo() {
   return new Promise (function (resolve, reject) {
     $.ajax({
       url: "http://www.omdbapi.com/?t=Home+Alone&y=&plot=short&r=json",
     }).done (function(data){
-      console.log(data);
-      console.log(data.Actors);
       resolve(data);
     });
   });
 }
 
 getMovieInfo();
+
+
+function getMovieInfo(movieData) {
+  let searchedMovies = "";
+
+  for (var i = search.length - 1; i >= 0; i--) {
+    let searchedMovies = data.search.Title
+  }
+}
